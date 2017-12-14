93c30863ea2ad07d446a415ddfb4a4610311ad08
Aamen <chen.yes.man@gmail.com>
2017-12-14 16:37:42 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ var Enemy = function(x, y, speed) {
 
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     var enemy = Object.create(Enemy.prototype);
-    Character.call(enemy, x, y, 'images/enemy-bug.png');
+    Character.call(enemy, x, y, 'images/1.png');
     enemy.speed = speed;
     enemy.fat = 75;
     return enemy
@@ -53,7 +53,7 @@ let allEnemies = new Array();
 var getEnemies = function() {
     const _x = [-240, -20, -141, -240, 440];
     const _y = [235, 155, 75];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 15; i++) {
         let _Enemy = new Enemy(_x[Math.floor(Math.random() * _x.length)], _y[Math.floor(Math.random() * _y.length)], Math.floor(Math.random() * 10 + 1));
         // console.log(_x[Math.floor(Math.random() * _x.length)] + ' ' + _y[Math.floor(Math.random() * _y.length)]);
         // _Enemy.x = _x[Math.floor(Math.random() * _x.length)];
@@ -66,7 +66,7 @@ var getEnemies = function() {
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
 var Player = function() {
-    this.sprite = 'images/char-boy.png';
+    this.sprite = 'images/2.png';
 }
 Player.prototype.update = function() {
 
